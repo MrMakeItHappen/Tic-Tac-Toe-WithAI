94c2805d82d63ba245bfa2b9d3e87b3ea92f890a
Stanley Miller <smiller8203@gmail.com>
2021-05-01 10:34:05 -0400

    Initial Commit

---
+++ Tic-Tac-Toe-WithAI/Tic_Tac_Toe_WithAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_WithAIApp.swift
+//  Tic-Tac-Toe-WithAI
+//
+//  Created by Stanley Miller on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_WithAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
